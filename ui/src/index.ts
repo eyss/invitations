3064cbe7d21118bb9e885baba76003ed55f2a58b
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-04-15 17:48:45 +0200

    Scaffolded code

---
+++ ui/src/index.ts
@@ -0,0 +1,4 @@
+export { CreateInvitation } from './elements/create-invitation';
+export { ReceivedInvitations } from './elements/received-invitations';
+export { InvitationsService } from './invitations.service';
+export { InvitationsStore } from './invitations.store';
