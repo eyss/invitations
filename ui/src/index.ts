7a8f4024d746a8c35a87e0df70071fc032f03dbe
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-06-21 18:53:27 +0200

    Removed import

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,4 +1,3 @@
-import'@webcomponents/scoped-custom-element-registry';
 export { InvitationsList } from './components/invitation-list';
 export { CreateInvitation } from './components/create-invitation';
 export { InvitationsStore } from './invitations.store';
