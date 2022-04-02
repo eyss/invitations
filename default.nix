ed61373560ff12abf0813f545e3fcd8f6b892b61
Thomas Cal <nphias@gmail>
2022-04-02 10:20:20 -0600

    upgraded to hdk 0.0.127

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
-  holonixRev = "46297548fd8db19bec228171a524330e7b5f6744";
+  holonixRev = "f42446be6102b02d152a70b4db87c869116c1a22";
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_124";
+    holochainVersionId = "v0_0_132";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
